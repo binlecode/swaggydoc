1a4f2bbb5a6737d8eb9a440aa349e95c2cdcbf98
Bin Le <bin.le@construction.com>
2017-10-02 17:15:49 -0400

    temporarily disable bintryUpload

--- travis.sh
+++ travis.sh
@@ -3,13 +3,11 @@ set -e
 rm -rf *.zip
 ./bowerize.sh
 
-filename=$(find . -name "grails-*.zip" | head -1)
-filename=$(basename $filename)
 
-echo "Publishing plugin 'swaggydoc' version $version"
-
-./gradlew \
-    swaggydoc-commons:bintrayUpload \
-    swaggydoc-grails3:bintrayUpload \
-    --info || echo "Bintray upload failed"
-
+#echo "Publishing plugin 'swaggydoc' version $version"
+#
+#./gradlew \
+#    swaggydoc-commons:bintrayUpload \
+#    swaggydoc-grails3:bintrayUpload \
+#    --info || echo "Bintray upload failed"
+#
