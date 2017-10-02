b765f65b492b02169d41ff4f228f69a8250c538c
Bin Le <bin.le@construction.com>
2017-10-02 15:06:36 -0400

    add first custom url in sample app, more to come

--- swaggydoc-grails3-example/grails-app/controllers/UrlMappings.groovy
+++ swaggydoc-grails3-example/grails-app/controllers/UrlMappings.groovy
@@ -2,11 +2,7 @@ class UrlMappings {
 
     static mappings = {
 
-
-        //fixme: this reveals a bug in swaggyDataSerivce swaggy spec json generation logic that a custom action
-        //fixme:   url mapping will supersede conventional CRUDs url paths by prefixing this custom action to
-        //fixme:   all url paths
-        '/book/customIndex'(controller: 'book', action: 'index')
+        '/customBook/customIndex'(controller: 'book', action: 'index')
 
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
