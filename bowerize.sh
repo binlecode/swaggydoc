58ecc6d563c6c8fb6e34acf5245003e623c59f40
Bin Le <bin.le@construction.com>
2017-10-02 16:13:15 -0400

    remove

--- bowerize.sh
+++ bowerize.sh
@@ -4,18 +4,6 @@ bower install
 
 BOWER_DIR=bower-sources/swagger-ui/dist/
 
-WA_DIR=grails2/web-app
-
-rm -rf $WA_DIR/images/ $WA_DIR/css/ $WA_DIR/js/
-mkdir -p $WA_DIR/images/
-mkdir -p $WA_DIR/css/
-mkdir -p $WA_DIR/js/swagger-lib/
-
-cp -R $BOWER_DIR/images/*      $WA_DIR/images/
-cp -R $BOWER_DIR/css/*         $WA_DIR/css/
-cp -R $BOWER_DIR/lib/*         $WA_DIR/js/swagger-lib/
-cp -R $BOWER_DIR/swagger-ui.js $WA_DIR/js/
-
 ASSETS=swaggydoc-grails3/grails-app/assets
 rm -rf $ASSETS/images/ $ASSETS/stylesheets/ $ASSETS/javascripts/
 mkdir -p $ASSETS/images/
