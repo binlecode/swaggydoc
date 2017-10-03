11cc12a13adcd08a2056a651d6476df087b273e2
Bin Le <bin.le@construction.com>
2017-10-02 22:03:44 -0400

    update app yml and logback for Grails 3.2.3

--- swaggydoc-grails3-example/grails-app/conf/logback.groovy
+++ swaggydoc-grails3-example/grails-app/conf/logback.groovy
@@ -1,25 +1,29 @@
 import grails.util.BuildSettings
 import grails.util.Environment
+import org.springframework.boot.logging.logback.ColorConverter
+import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
+
+import java.nio.charset.Charset
+
+conversionRule 'clr', ColorConverter
+conversionRule 'wex', WhitespaceThrowableProxyConverter
 
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
-
-def logPattern = '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
-        '%clr(%5p) ' + // Log level
-        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
-        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
-        '%m%n%wex' // Message
-
-
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = logPattern
+        charset = Charset.forName('UTF-8')
+
+        pattern =
+                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+                        '%clr(%5p) ' + // Log level
+                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                        '%m%n%wex' // Message
     }
 }
 
-root(INFO, ['STDOUT'])
-
 def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir) {
+if (Environment.isDevelopmentMode() && targetDir != null) {
     appender("FULL_STACKTRACE", FileAppender) {
         file = "${targetDir}/stacktrace.log"
         append = true
@@ -28,10 +32,13 @@ if (Environment.isDevelopmentMode() && targetDir) {
         }
     }
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+    root(ERROR, ['STDOUT', 'FULL_STACKTRACE'])
+}
+else {
+    root(ERROR, ['STDOUT'])
 }
 
 def loggerList = ['STDOUT']
-
 logger('grails.app.controllers', DEBUG, loggerList, false)
 logger('grails.app.services', DEBUG, loggerList, false)
 logger('grails.app.jobs', DEBUG, loggerList, false)
