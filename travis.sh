095d996efd2c4b6cb25511863db6975e95baec5e
Bin Le <bin.le@construction.com>
2017-10-05 10:28:16 -0400

    update travis script for integrated codebase

--- travis.sh
+++ travis.sh
@@ -3,11 +3,16 @@ set -e
 rm -rf *.zip
 ./bowerize.sh
 
+echo "building plugin and example Grails app"
+./gradlew \
+    swaggydoc-grails3:assemble \
+    swaggydoc-grails3-example:assemble \
+    --info || echo "Bintray upload failed"
 
 #echo "Publishing plugin 'swaggydoc' version $version"
 #
 #./gradlew \
 #    swaggydoc-commons:bintrayUpload \
 #    swaggydoc-grails3:bintrayUpload \
-#    --info || echo "Bintray upload failed"
+#
 #
